8ab6ddfc8760f6ea1353d060a6c731637939a277
Leonardo Mendonca <leo.bmendonca@gmail.com>
2022-03-29 23:06:06 -0300

    Pagina Home finalizada, rotas estruturadas

--- src/App.js
+++ src/App.js
@@ -1,11 +1,17 @@
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import About from './pages/About';
 import Home from './pages/Home';
+import Projects from './pages/Projects';
+import Technologies from './pages/Technologies';
 
 function App() {
   return (
     <BrowserRouter>
       <Routes>
         <Route exact path="" element={ <Home /> } />
+        <Route path="/projects" element={ <Projects /> } />
+        <Route path="/about" element={ <About /> } />
+        <Route path="/technologies" element={ <Technologies /> } />
       </Routes>
     </BrowserRouter>
   );
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,39 +1,54 @@
 import React, { useEffect, useState } from 'react';
+import { Link } from 'react-router-dom';
 import profilePic from '../images/profile.png';
 
 function Home() {
-  const [hrStyle, setHrStyle] = useState('w-1 h-2')
-  const [upContainerStyle, setUpContainerStyle] = useState('opacity-0 translate-y-60')
+  const [hrStyle, setHrStyle] = useState('w-1 h-2');
+  const [upContainerStyle, setUpContainerStyle] = useState('opacity-0 translate-y-52');
+  const [downContainerStyle, setDownContainerStyle] = useState('opacity-0 -translate-y-52 ')
 
+  const MENU_LINKS_STYLE = "ease-in duration-200 mx-20 rounded-xl p-2 hover:bg-slate-100 hover:text-black";
+  
   useEffect(() => {
     setTimeout(() => {
       setHrStyle('w-5/6 h-2');
       setUpContainerStyle('opacity-0');
-    }, 800)
+    }, 800);
     setTimeout(() => {
       setUpContainerStyle('');
-    }, 920)
+    }, 1040);
+    setTimeout(() => {
+      setDownContainerStyle('');
+    }, 1040);
   }, [])
 
   return (
     <main className="flex flex-col justify-center items-center h-full">
       <div
-        className="h-full w-full flex items-center justify-center"
+        className={`ease-in-out duration-700 h-full w-full flex items-center justify-center ${upContainerStyle}`}
       >
-        <div className={`ease-in-out duration-500 mx-10 flex flex-col ${upContainerStyle}`}>
-          <h1 className="text-center font-bold text-6xl text-white">LEONARDO</h1>
-          <h1 className="text-center text-[2.15rem] text-white">BARROS MENDONÇA</h1>
+        <div className="mx-20 flex flex-col">
+          <h1 className="text-center font-bold text-7xl text-white">LEONARDO</h1>
+          <h1 className="text-center text-[2.5rem] text-white">BARROS MENDONÇA</h1>
         </div>
         <img
-          className={`ease-in-out duration-500 mx-10 rounded-full w-72 ${upContainerStyle}`}
+          className="ease-in-out duration-700 mx-20 rounded-full w-72"
           src={profilePic} alt="Leonardo"
         />
       </div>
       <hr
         className={`ease-in-out duration-500 rounded-sm border-none bg-slate-100 z-10 ${hrStyle}`}
       />
-      <nav className="h-full z-10 bg-red-100">
-        
+      <nav className={`ease-in-out duration-700 flex items-center text-white font-semibold text-4xl h-full z-10 ${downContainerStyle}`}>
+        <Link to="/projects">
+          <h2 className={ MENU_LINKS_STYLE }>Projects</h2>
+        </Link>
+        <Link to="/about">
+          <h2 className={ MENU_LINKS_STYLE }>About</h2>
+        </Link>
+        <Link to="technologies">
+          <h2 className={ MENU_LINKS_STYLE }>Technologies</h2>
+        </Link>
       </nav>
     </main>
 
