256b8e443c5d8b884c0fea6b77ce14b37237afb8
Leonardo Mendonca <leo.bmendonca@gmail.com>
2022-04-11 11:31:44 -0300

    feat: trybewarts form video added on projects page

--- src/pages/Projects.js
+++ src/pages/Projects.js
@@ -3,6 +3,7 @@ import { useNavigate } from 'react-router-dom';
 import MenuNav from '../components/MenuNav';
 import ProjectCard from '../components/ProjectCard';
 import trybeTunesVideo from '../midia/Trybe Tunes.mp4';
+import trybewartsFormVideo  from '../midia/Trybewarts Form.mp4';
 
 function Projects() {
   const [hrStyle, setHrStyle] = useState('w-1 h-2');
@@ -46,7 +47,7 @@ function Projects() {
         className={`ease-in-out duration-700 h-full w-full flex items-center justify-evenly ${upContainerStyle}`}
       >
         <ProjectCard title="Trybe Tunes" videoSrc={ trybeTunesVideo } />
-        <ProjectCard title="Trybe Tunes" videoSrc={ trybeTunesVideo } />
+        <ProjectCard title="Trybe Tunes" videoSrc={ trybewartsFormVideo } />
       </div>
       <hr
         className={`ease-in-out duration-500 rounded-sm border-none bg-slate-100 z-10 ${hrStyle}`}
