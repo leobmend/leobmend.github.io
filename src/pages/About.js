de8ae08d26ebbb733725923deb515fdaf4018ae4
Leonardo Mendonca <leo.bmendonca@gmail.com>
2022-04-12 07:16:25 -0300

    feat: responsive design on About page to small devices

--- src/pages/About.js
+++ src/pages/About.js
@@ -46,12 +46,12 @@ function About() {
   }
 
   return (
-    <main className="flex justify-center items-center h-full overflow-hidden">
+    <main className="flex justify-center items-center h-full overflow-hidden flex-col sm:flex-row">
       <MenuNav
         handleClick={ handleClick } menuContainerStyles={ menuContainerStyle } actualPage="About"
       />
       <section
-        className={`ease-in-out duration-500 h-full py-20 flex flex-col items-center justify-around ${leftContainerStyle}`}
+        className={`ease-in-out duration-500 h-full py-20 hidden md:flex flex-col items-center justify-around ${leftContainerStyle}`}
       >
         <img
           className="ease-in-out duration-500 rounded-full w-9/12 shadow-sm shadow-gray-50"
@@ -94,27 +94,30 @@ function About() {
         </section>
       </section>
       <hr
-        className={`ease-in-out duration-500 rounded-sm border-none bg-slate-100 ${hrStyle}`}
+        className={`ease-in-out duration-500 rounded-sm border-none bg-slate-100 ${hrStyle} hidden md:block`}
       />
       <article
         className={`ease-in-out duration-700 h-full w-full flex flex-col text-white items-center justify-center ${rightContainerStyle}`}
       >
-        <div className="mt-auto px-40 ml-10 self-start">
-          <h1 className="text-4xl text-white">Hi! I'm</h1>
-          <h1 className="font-bold text-6xl text-white">LEONARDO</h1>
+        <div className="flex mt-auto items-center justify-center md:justify-start md:px-40 md:ml-10 w-full">
+          <img className="rounded-full w-1/3 mr-8 md:hidden" src={profilePic} alt="Leonardo"/>
+          <div>
+            <h1 className="self-start text-white text-xl md:text-4xl ">Hi! I'm</h1>
+            <h1 className="font-bold text-white text-2xl md:text-6xl">LEONARDO</h1>
+          </div>
         </div>
-        <div className="my-auto px-40">
-          <p className="text-2xl">
+        <div className="my-auto text-justify overflow-auto h-1/2 md:h-fit text-md md:text-2xl px-8 md:px-40 scrollbar">
+          <p>
             I'm a peaceful guy, with an easy laugh. Always trying to see the good side of situations, or just making jokes with them. I love a good movie, book or tv show, and I dedicate myself to my friendships always that I have an opportunity. Play video games are definitely my hooby: there isn't something like a couch, joysticks and some friends.
             {/* Sou alguém tranquilo e de riso fácil. Sempre tento ver o ponto positivo das situações, ou apenas fazer piadas com elas. Adoro um bom filme ou seriado, e cultivo minhas amizades sempre que posso. Video-games é o meu principal hobbie: nada como um sofá e um controle. */}
           </p>
           <br />
-          <p className="text-2xl">
+          <p>
             Technology enthusiast since my childhood, I ended up choosing for a graduation course not so related. However, certain things in life choose us, instead of us choosing them. And now I'm here persuing the dream of make a career on software development. Therefore, every day I seek to learn more about programming and its ability to change the world.
             {/* Entusiasta de tecnologia desde a infância, acabei por optar por uma graduação não tão ligada à área. Porém, certas coisas na nossa vida nos escolhem, ao invés de as escolhermos. E agora persigo o sonho de trilhar uma carreira em desenvolvimento de software. Portanto, busco cada dia saber um pouco mais sobre programação e a sua capacidade de mudar o mundo. */}
           </p>
           <br />
-          <p className="text-2xl">
+          <p>
             Currently, I participate in the
             {' '}
             <a className="font-bold" target="_blank" rel="noreferrer" href="https://www.youtube.com/watch?v=QxDZSwtMJZ4">Turma XP</a>
@@ -135,7 +138,7 @@ function About() {
             com a
             {' '}
             <a className="font-bold" target="_blank" rel="noreferrer" href="https://www.xpinc.com/">XP Inc</a>.
-          O curso contempla dos fundamentos até os frameworks mais utilizados pelo mercado de desenvolvimento web, além de preparar o profissional para o mercado de trabalho, treinando-o em diversas <span className="italic">soft-skills</span>. */}
+            O curso contempla dos fundamentos até os frameworks mais utilizados pelo mercado de desenvolvimento web, além de preparar o profissional para o mercado de trabalho, treinando-o em diversas <span className="italic">soft-skills</span>. */}
           </p>
         </div>
       </article>
